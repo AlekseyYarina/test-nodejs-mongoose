2b12975bfb7538fe63fa4cd5e79beb06b7b68238
Yarina Oleksii <aleksey.yarina@gmail.com>
2024-05-01 20:57:58 +0300

    +

---
+++ index.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const DB_URI = process.env.DB_URI;
+async function run() {
+  try {
+    await mongoose.connect(DB_URI);
+    console.info("Database connection succesfully");
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+run().catch((error) => console.error(error));
